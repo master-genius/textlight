a8abf7e971ffbda341958961d8690575073c8173
BraveWang <1146040444@qq.com>
2019-11-12 21:06:25 +0800

    --

--- controller/api/banner.js
+++ controller/api/banner.js
@@ -13,7 +13,7 @@ class banner {
 
     try {
       c.res.body.banner = await new Promise((rv, rj) => {
-        fs.readdir(c.service.sitedata+'/banner', (err, flist) => {
+        fs.readdir(c.service.sitedata+'/image/banner', (err, flist) => {
           if (err) {
             rj(err);
           } else {
@@ -27,7 +27,7 @@ class banner {
 
     try {
       let detail = await c.service.funcs.readFile(
-          c.service.sitedata+'/banner.json');
+          c.service.sitedata+'/box/banner.json');
       c.res.body.detail = JSON.parse(detail);
     } catch (err) {
       c.res.body.detail = null;
